c85ae144ac1f12efbf58ecdb41e1f54c9412145f
David Ryan <11883046+davidryan59@users.noreply.github.com>
2025-07-12 09:35:09 +0700

    Add melody generation classes

---
+++ classes/melody_generation.py
@@ -0,0 +1,34 @@
+from typing import Union, List
+from dataclasses import dataclass
+from fractions import Fraction
+
+
+# Classes to iteratively build up melodies as graphs of freq vs time
+
+
+@dataclass
+class NoteBase:
+    # Abstract base class
+    t: Union[Fraction, int]  # Duration, unit could be seconds, or beats
+
+
+@dataclass
+class Note(NoteBase):
+    # Represents a frequency (f), at a volume (db)
+    f: Union[Fraction, int]  # Frequency, unit could be Hz, or relative
+    db: Union[Fraction, int] = 100  # Decibels, where 100 dB = amplitude 1
+
+
+@dataclass
+class Rest(NoteBase):
+    # Represents a rest, which has a duration, nothing else
+    pass
+
+
+@dataclass
+class Melody:
+    # Represents a list of notes (or rests) to be played sequentially
+    notes: List[NoteBase]
+
+    def duration(self):
+        return sum([n.t for n in self.notes])
